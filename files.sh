5b8e8d0c31a01afe6cc6240a6c8460bd90206cc3
hassanShakur <hass.shakurhassan@gmail.com>
2023-06-10 18:00:17 +0300

    Initial commit

---
+++ files.sh
@@ -0,0 +1,12 @@
+# File & dir creation
+touch fileName
+mkdir dirName
+
+# Display files & dirs sorted according to creation time
+ls -lt
+
+# To reverse the order, add r
+ls -ltr
+
+# inodes => Pointers to file location in memory -  The i is for inode
+ls -ltri
