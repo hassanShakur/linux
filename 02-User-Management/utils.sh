83e3744e034e938177c206c0926970096d34b9f1
hassanShakur <hass.shakurhassan@gmail.com>
2023-06-15 15:46:43 +0300

    Sys utils & processes

---
+++ 02-User-Management/utils.sh
@@ -0,0 +1,18 @@
+# Uptime
+uptime
+
+# Host
+hostname
+
+# Unix machine you are running
+uname
+
+# Location of command scripts - mostly /usr/bin
+which pwd
+which ls
+
+# Calender
+cal
+
+# Calculator
+bc
---
+++ 03-Processes/process.sh
@@ -0,0 +1,25 @@
+# Start ot stop an app. Or even schedule apps that start when sys is booted
+systemctl...
+
+# Daemons are background processes like ntp which is for time management. These process names end with d like ntpd
+
+# View currently running processes
+ps -ef
+
+# restart a process eg ntpd
+systemctl restart ntpd
+
+# Status of a process
+systemctl status ntpd
+
+# Stop process
+systemctl stop ntpd
+
+# Or
+kill PID
+
+# Start service on bootup
+systemctl enable ntpd
+
+# List of processes and resource usage
+top
