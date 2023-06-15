dd5a84735e607e7254ef180847202e262c282de3
hassanShakur <hass.shakurhassan@gmail.com>
2023-06-15 14:08:03 +0300

    Users communication

---
+++ 02-User-Management/communication.sh
@@ -0,0 +1,11 @@
+# Check all users logged in
+users
+
+# Messaging logged in users
+wall # Enter
+# Type your msg then hit Ctrl+D to send
+
+# Writing to 1 user
+write dragon # Enter
+# Type the message. Whenever you press enter the line in sent
+# The user on the other end can also send you messages while you haven't hit Ctrl+D
---
+++ 02-User-Management/userMonitoring.sh
@@ -0,0 +1,11 @@
+# Users currently logged in
+who
+
+# All who have logged in during a period, whether sys crashed...
+last
+
+# Logged in users, what time they logged in, idle time, cpu usage...
+w
+
+# This is also similar
+finger
